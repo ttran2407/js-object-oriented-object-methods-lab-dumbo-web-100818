1c9199192e906a8435f5e6b36d240aeb8f4e3640
Tien Tran <tientran2407.vn@gmail.com>
2019-04-12 21:38:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,11 @@ function BroadMember (name, homeState, training) {
   
   this.doCharity = () => {console.log("I like to help people.")};
   
+<<<<<<< HEAD
   this.releasePressStatement = () => {console.log"(You will see great things from Scuber.")};
+=======
+  this.releasePressStatement = () => {console.log("You will see great things from Scuber.")};
+>>>>>>> bc54330d23fc5e46cb15867ddbcf65a2541309c1
   
   this.sayHi = () => {console.log(`Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`)};
 }
